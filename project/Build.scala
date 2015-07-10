7995229b4be2a4b1ebf8bfb887608e17563eadff
snowgooseyk <snowgoose.yk@gmail.com>
2015-07-11 00:05:25 +0900

    Fix worng version.

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,7 @@ object BuildSettings {
     val buildOrganization = "com.github.snowgooseyk"
     val buildVersion      = "0.1.0-SNAPSHOT"
     val buildScalaVersion = "2.11.7"
-    val clossBuildScalaVersion = Seq("2.10.6","2.11.7")
+    val clossBuildScalaVersion = Seq("2.10.5","2.11.7")
 
     val buildSettings = Defaults.defaultSettings ++ ReleasePlugin.releaseSettings ++ sonatypeSettings ++ ScoverageSbtPlugin.projectSettings  ++ Seq (
       organization := buildOrganization,
@@ -52,15 +52,15 @@ object Dependencies {
 
   val commonsLang = "org.apache.commons" % "commons-lang3" % "3.4"
   val specsCore =  "org.specs2" % "specs2-core" % specsVersion % "test" cross CrossVersion.fullMapped {
-      case "2.10.6" => "2.10"
+      case "2.10.5" => "2.10"
       case "2.11.7" => "2.11"
   }
   val specsJunit =  "org.specs2" % "specs2-junit" % specsVersion % "test" cross CrossVersion.fullMapped {
-      case "2.10.6" => "2.10"
+      case "2.10.5" => "2.10"
       case "2.11.7" => "2.11"
   }
   val specsMock =  "org.specs2" % "specs2-mock" % specsVersion % "test" cross CrossVersion.fullMapped {
-      case "2.10.6" => "2.10"
+      case "2.10.5" => "2.10"
       case "2.11.7" => "2.11"
   }
 
